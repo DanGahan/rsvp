922858b08d07f074b789776fd6124d672a458b59
Dan Gahan <dan@dans-mac-mini.home>
2024-01-11 22:04:39 +0000

    sorted form alignment and used jquery to disable plus_one_name unless plus_one value is yes

--- frontend/frontend/static/js/form.js
+++ frontend/frontend/static/js/form.js
@@ -1,36 +1,11 @@
-// frontend/static/js/form.js
-
 $(document).ready(function() {
-    $('#submitForm').submit(function(event) {
-        // Prevent the default form submission behavior
-        event.preventDefault();
-
-        // Gather the form data
-        var formData = {
-            'name': $('#name').val(),
-            'plus_one': $('#plus_one').val(),
-            'plus_one_name' $('#plus_one_name').val(),
-            'song_suggestion' $('#song_suggestion').val(),
-            // Add other form fields as needed
-        };
-
-        // Make an AJAX request to the backend API
-        $.ajax({
-            type: 'POST',
-            url: 'http://backend:5000/rsvp/',  // Update the URL to match your backend API endpoint
-            data: formData,
-            dataType: 'json',
-            success: function(response) {
-                // Handle the successful API response
-                console.log(response);
-                alert('Data submitted successfully!');
-            },
-            error: function(error) {
-                // Handle the API error
-                console.log(error);
-                alert('An error occurred while submitting the data.');
-            }
-        });
+    // Event handler when the document is ready
+    $('#plus_one').on('change', function() {
+        var plusOneValue = $(this).val();
+        if (plusOneValue === 'Yes') {
+            $('#plus_one_name').prop('disabled', false);
+        } else {
+            $('#plus_one_name').prop('disabled', true);
+        }
     });
-});
-
+  });
