7652bacc5ef4e24b83e5dbc56646996a866a3f34
Dan Gahan <dan@dans-mac-mini.home>
2024-01-16 15:57:29 +0000

    removed .sql from git ignore as was stopping init-db.sql from being committed

---
+++ db/init-db.sql
@@ -0,0 +1,22 @@
+-- Connect to the database
+\c rsvp_app;
+
+-- Create the rsvps table
+CREATE TABLE rsvps (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    attending VARCHAR(10) NOT NULL,
+    vegetarian VARCHAR(10),
+    plus_one VARCHAR(10),
+    plus_one_name VARCHAR(100),
+    plus_one_vegetarian VARCHAR(10),
+    song_suggestion VARCHAR(200)
+);
+
+CREATE TABLE evening_rsvps (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    attending VARCHAR(10) NOT NULL,
+    plus_one VARCHAR(10),
+    plus_one_name VARCHAR(100)
+);
