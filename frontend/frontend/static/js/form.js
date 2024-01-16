2d44eb6d0d22f528ad6fb9d006da4510081130a0
Dan Gahan <dan@dans-mac-mini.home>
2024-01-16 08:41:48 +0000

    updated javascript form.js logic to enable elements based on previous suggestions

--- frontend/frontend/static/js/form.js
+++ frontend/frontend/static/js/form.js
@@ -1,12 +1,53 @@
 $(document).ready(function() {
-    // Event handler when the document is ready
-
-    $('#plus_one').on('change', function() {
-        var plusOneValue = $(this).val();
-        if (plusOneValue === 'Yes') {
-            $('#plus_one_name').prop('disabled', false);
+    // Initialize the form elements
+    var attendingSelect = $("#attending");
+    var vegetarianSelect = $("#vegetarian");
+    var plusOneSelect = $("#plus_one");
+    var plusOneNameInput = $("#plus_one_name");
+    var plusOneVegetarianSelect = $("#plus_one_vegetarian");
+    var songSuggestionInput = $("#song_suggestion");
+  
+    // Disable initial form elements
+    vegetarianSelect.prop("disabled", true).addClass("disabled");
+    plusOneSelect.prop("disabled", true).addClass("disabled");
+    plusOneNameInput.prop("disabled", true).addClass("disabled");
+    plusOneVegetarianSelect.prop("disabled", true).addClass("disabled");
+    songSuggestionInput.prop("disabled", true).addClass("disabled");
+  
+    // Enable or disable form elements based on the selected values
+    attendingSelect.on("change", function() {
+      var attendingValue = attendingSelect.val();
+  
+      if (attendingValue === "Yes") {
+        vegetarianSelect.prop("disabled", false).removeClass("disabled");
+        plusOneSelect.prop("disabled", false).removeClass("disabled");
+  
+        if (plusOneSelect.val() === "Yes") {
+          plusOneNameInput.prop("disabled", false).removeClass("disabled");
+          plusOneVegetarianSelect.prop("disabled", false).removeClass("disabled");
         } else {
-            $('#plus_one_name').prop('disabled', true);
+          plusOneNameInput.prop("disabled", true).addClass("disabled");
+          plusOneVegetarianSelect.prop("disabled", true).addClass("disabled");
         }
+  
+        songSuggestionInput.prop("disabled", false).removeClass("disabled");
+      } else {
+        vegetarianSelect.prop("disabled", true).addClass("disabled");
+        plusOneSelect.prop("disabled", true).addClass("disabled");
+        plusOneNameInput.prop("disabled", true).addClass("disabled");
+        plusOneVegetarianSelect.prop("disabled", true).addClass("disabled");
+        songSuggestionInput.prop("disabled", true).addClass("disabled");
+      }
+    });
+  
+    // Enable or disable plus_one_name and plus_one_vegetarian based on plus_one value
+    plusOneSelect.on("change", function() {
+      if (attendingSelect.val() === "Yes" && plusOneSelect.val() === "Yes") {
+        plusOneNameInput.prop("disabled", false).removeClass("disabled");
+        plusOneVegetarianSelect.prop("disabled", false).removeClass("disabled");
+      } else {
+        plusOneNameInput.prop("disabled", true).addClass("disabled");
+        plusOneVegetarianSelect.prop("disabled", true).addClass("disabled");
+      }
     });
   });
